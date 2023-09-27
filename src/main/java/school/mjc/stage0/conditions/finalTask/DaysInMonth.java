a48e89e18c49e25a86c58e3a72c6a50911042e6e
fenix <fenixtheidragon@gmail.com>
2023-09-27 17:32:03 +0300

    4.1 Final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,30 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        boolean leapOrNot;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            leapOrNot = true;
+        } else {
+            leapOrNot = false;
+        }
+        String result;
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                result = "31";
+                break;
+            case 4, 6, 9, 11:
+                result = "30";
+                break;
+            case 2:
+                if (leapOrNot) {
+                    result = "29";
+                } else {
+                    result = "28";
+                }
+                break;
+            default:
+                result = "invalid date";
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        String result;
+        switch (monthNumber) {
+            case 1, 2, 12:
+                result = "Winter";
+                break;
+            case 3,4,5:
+                result = "Spring";
+                break;
+            case 6,7,8:
+                result = "Summer";
+                break;
+            case 9,10,11:
+                result = "Autumn";
+                break;
+            default:
+                result = "Wrong month number";
+        }
+        System.out.println(result);
     }
 }
